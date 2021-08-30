e209370a2d72e11413ee17df606452aed889b20b
Ilya Libin <libin.ilya@gmail.com>
2021-08-30 09:23:54 +0300

    The how it works modal is working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import {
   ChakraProvider,
   Box,
@@ -7,31 +9,55 @@ import {
   VStack,
   Code,
   Grid,
-  theme,
+  Flex,
+  Spacer,
+  Button,
+  useDisclosure
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
+import theme from './helpers/config'
+import Matrix from './components/Matrix';
+import HowItWorksModal from './components/HowItWorksModal';
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleHowItWorksClose = () => {
+    localStorage.setItem('pixellord-onboarding-seen', 'yes');
+    onClose()
+  }
+  useEffect(() => {
+    const isSeen = localStorage.getItem('pixellord-onboarding-seen');
+    if (!isSeen) { onOpen() }
+  });
+
   return (
     <ChakraProvider theme={theme}>
+      <Flex pt={5} pb={5} width={1000} style={{ margin: '0 auto'}}>
+        <h1>The Million ADA Homepage</h1>
+        <Spacer />
+        <Box p="2">
+          <Link
+            textDecoration="underline"
+            href="#"
+            onClick={onOpen}
+            fontSize="3xlg"
+            fontWeight="bold"
+            justifySelf="flex-start"
+            pr={10}>
+            How it works
+          </Link>
+          <Button colorScheme="teal" justifySelf="flex-end">Connect your wallet</Button>
+        </Box>
+      </Flex>
+      <HowItWorksModal isOpen={isOpen} onOpen={onOpen} onClose={handleHowItWorksClose} />
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
+          <Matrix />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
             <Text>
               Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
             </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
           </VStack>
         </Grid>
       </Box>
